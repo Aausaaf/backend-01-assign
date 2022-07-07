233b0d3300a0715e68b885258efcd4749928a426
Aausaf Alam <aausafalam585@gmail.com>
2022-07-06 18:48:47 -0700

    solve

---
+++ main.js
@@ -0,0 +1,11 @@
+const Add  = require("./add")
+const Sub = require("./sub")
+const Mul = require("./mul")
+const Div = require("./division")
+
+
+
+console.log(Add(2,3))
+console.log(Sub(5,4))
+console.log(Mul(4,5))
+console.log(Div(3,6))
